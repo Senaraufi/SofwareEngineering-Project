a7ae13c22171a106e301df91ba894488afe1173e
sena <88288148+Senaraufi@users.noreply.github.com>
2025-02-25 16:35:31 +0000

    Created start.sh
    Its easier to run the project with this script.

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Kill any existing PHP servers and Tailwind processes
+pkill -f 'php -S' 2>/dev/null
+pkill -f tailwindcss 2>/dev/null
+
+# Start Tailwind in the background
+cd "$(dirname "$0")/frontend" && node_modules/.bin/tailwindcss -i ../public/css/input.css -o ../public/css/style.css --watch &
+
+# Start PHP server
+cd "$(dirname "$0")/public" && php -S localhost:8000
+
+# This function will be called when the script is interrupted
+cleanup() {
+    echo "Stopping servers..."
+    pkill -f 'php -S'
+    pkill -f tailwindcss
+    exit 0
+}
+
+# Set up the cleanup function to run when the script receives an interrupt signal
+trap cleanup INT
