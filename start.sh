77ccb64c78d87eec22a598833438e07351fea255
sena <88288148+Senaraufi@users.noreply.github.com>
2025-03-02 20:14:07 +0000

    start.sh script

--- start.sh
+++ start.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Kill any existing PHP servers and Tailwind processes
 pkill -f 'php -S' 2>/dev/null
 pkill -f tailwindcss 2>/dev/null
 
-# Start Tailwind in the background
 cd "$(dirname "$0")/frontend" && node_modules/.bin/tailwindcss -i ./assets/css/input.css -o ./assets/css/style.css --watch &
 
-# Start PHP server
 cd "$(dirname "$0")/public" && php -S localhost:8000
 
-# This function will be called when the script is interrupted
 cleanup() {
     echo "Stopping servers..."
     pkill -f 'php -S'
@@ -18,5 +14,4 @@ cleanup() {
     exit 0
 }
 
-# Set up the cleanup function to run when the script receives an interrupt signal
 trap cleanup INT
