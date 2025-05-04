a62db8d120adb09f81bf6bd5a49fd0662c3339a7
Pixie <B00159841@mytudublin.ie>
2025-05-04 23:05:29 +0100

    Still trying to fix issues

--- backend/src/config/database.php
+++ backend/src/config/database.php
@@ -7,10 +7,10 @@
  */
 
 return [
-    'host' => 'localhost:3307', // the new port 
+    'host' => 'localhost', // Reverting to default
     'dbname' => 'talktempo',
     'username' => 'root',
-    'password' => '',       // Updated this just in case MySQL has a password
+    'password' => '',
     'charset' => 'utf8mb4',
     'options' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,// Set error mode to exception
